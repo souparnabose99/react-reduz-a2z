a0b77ce63419117505f59e5e7bad505f8fba4c41
Souparna Bose <souparnabose99@gmail.com>
2024-06-24 08:48:24 +0530

    Update project files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+// React Steps:
+
+// 1-> Import React and ReactDOM libraries
+// 2-> Get a reference to the div with ID root
+// 3-> Tell React to take control of that element
+// 4-> Create a Component
+// 5-> Show the Component on screen
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
