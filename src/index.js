8d08784a214ffbeaf7eac9ea9749a457542ee2f9
Souparna Bose <souparnabose99@gmail.com>
2024-06-25 23:02:33 +0530

    Update project files

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
 // React Steps:
 
 // 1-> Import React and ReactDOM libraries
-// 2-> Get a reference to the div with ID root
-// 3-> Tell React to take control of that element
-// 4-> Create a Component
-// 5-> Show the Component on screen
-
+import { type } from '@testing-library/user-event/dist/type';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 2-> Get a reference to the div with ID root
+const element = document.getElementById('root')
+// 3-> Tell React to take control of that element
+const root = ReactDOM.createRoot(element);
+// 4-> Create a Component
+function App() {
+  /*
+  let message = "Bye there"
+  const name = "X"
+  if (Math.random > 0.5) {
+    message = "Hi there"
+    
+  }
+    */
+  //return <h1>Hello there {name}. {message} {name}.</h1>;
+  //return <h1>{new Date().toLocaleTimeString()}</h1>;
+  //return <input style={{border: '3px solid red'}} type='number' min={2} max={9}/>
+  return <textarea autoFocus={true}/>
+}
+// 5-> Show the Component on screen
+root.render(<App />)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
