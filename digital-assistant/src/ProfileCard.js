43a395fe49c53f171f3ba962a9152cf39b0f4d9a
Souparna Bose <souparnabose99@gmail.com>
2024-06-29 19:08:35 +0530

    Update digital-assistant

--- digital-assistant/src/ProfileCard.js
+++ digital-assistant/src/ProfileCard.js
@@ -1,6 +1,7 @@
-const ProfileCard = ({ title, handle }) => {
+const ProfileCard = ({ title, handle, image }) => {
     return (
         <div>
+            <img src={image} />
             <div>Title is {title}</div>
             <div>Handle is {handle}</div>
         </div>
