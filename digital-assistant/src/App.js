0e78d56137eb2f3a435c822a86bd7887d49bfe70
Souparna Bose <souparnabose99@gmail.com>
2024-06-29 19:06:05 +0530

    Update digital-assistant

--- digital-assistant/src/App.js
+++ digital-assistant/src/App.js
@@ -1,12 +1,15 @@
-import ProfileCard from "./ProfileCard";
+import ProfileCard from './ProfileCard';
+import AlexaImage from './images/alexa.png'
+import CortanaImage from './images/cortana.png'
+import SiriImage from './images/siri.png'
 
 const App = () => {
   return(
     <div>
       <div>Personal Digital Assistant</div>
-      <ProfileCard title='Alexa' handle='alexa@11'/>
-      <ProfileCard title='Cortana' handle='cortana@22'/>
-      <ProfileCard title='Siri' handle='siri@33'/>
+      <ProfileCard title='Alexa' handle='alexa@11' image={AlexaImage}/>
+      <ProfileCard title='Cortana' handle='cortana@22' image={CortanaImage}/>
+      <ProfileCard title='Siri' handle='siri@33' image={SiriImage}/>
       </div>
   )
 }
