4a232aeb1ac9d98ca9d1af84dc08fa0de9125075
Souparna Bose <souparnabose99@gmail.com>
2024-07-01 12:00:52 +0530

    Update digital-assistant

--- digital-assistant/src/App.js
+++ digital-assistant/src/App.js
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css'
 import ProfileCard from './ProfileCard';
 import AlexaImage from './images/alexa.png'
 import CortanaImage from './images/cortana.png'
@@ -7,6 +8,12 @@ const App = () => {
   return(
     <div>
       <div>Personal Digital Assistant</div>
+
+      <div className='container'>
+        <section className='section'>
+          <div className='columns'></div>
+        </section>
+      </div>
       <ProfileCard title='Alexa' handle='alexa@11' image={AlexaImage}/>
       <ProfileCard title='Cortana' handle='cortana@22' image={CortanaImage}/>
       <ProfileCard title='Siri' handle='siri@33' image={SiriImage}/>
